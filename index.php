1b3f65037d24dbefcda30a4f7a303d10f41900d8
LelioMarcos <leliomrcunha@gmail.com>
2021-12-06 09:26:31 -0300

    login

--- index.php
+++ index.php
@@ -1,6 +1,24 @@
 <?php 
 include_once "header.php";
 include_once 'dbconnect.php';
+
+session_start();
+
+// se não existir a sessão logado
+if (!isset($_SESSION['logado'])):
+	header('Location: login.php');
+endif;
+
+
+//Dados
+$id = $_SESSION['id_usuario'];
+$sql = "SELECT * FROM usuario WHERE idUsuario='$id'";
+$resultadoa= mysqli_query($connect,$sql);
+//transforma a variavel resultado em array 
+$dadoasds =mysqli_fetch_array($resultadoa);
+
+echo $dadoasds['nomUsuario'];
+//fechando a conexão depois de armazenar os dados
 ?>
 
 <!-- Prévia da história criada por um usuário -->
@@ -12,13 +30,18 @@ include_once 'dbconnect.php';
     if (mysqli_num_rows($resultado)>0):
         while($dados =mysqli_fetch_array($resultado)):	
             ?>
-    <div class="historia-preview">
-        <header class="title">
-            <?php echo $dados['nomHist'];?>
-        </header>
-        <hr>
-        <?php echo $dados['dscCorpoHist'];?>
-        </div>
+            <div class="historia-preview">
+                <header class="title">
+                    <?php echo $dados['nomHist'];?> | <?php 
+                    $sql="SELECT * FROM usuario WHERE idUsuario = " . $dados['idUsuario'] . ";";
+                    $resultadoUsuario = mysqli_query($connect,$sql);
+                    $usuario =mysqli_fetch_array($resultadoUsuario);
+                    echo $usuario['nomUsuario'];
+                    ?>
+                </header>
+                <hr>
+                <?php echo $dados['dscSinopseHist'];?>
+            </div>
         <?php endwhile; 
     endif;
     ?>
---
+++ login.php
@@ -0,0 +1,56 @@
+<?php 
+include_once "header.php";
+include_once 'dbconnect.php';
+//iniciar a sessão
+session_start();
+
+//se existir o indice btn_entrar , é porque alguem clicou no botão
+if (isset($_POST['btn-entrar'])):
+	//echo "Clicou";
+	$erros = array();
+	//mysqli_escape_string - função que limpa os dados e evita sqlinjection e outros caracteres indevidos.
+	$login = mysqli_escape_string($connect, $_POST['login']);
+	$senha = mysqli_escape_string($connect, $_POST['senha']);
+	
+	
+	if(empty($login) or empty($senha)):
+		$erros[] = "<li> O campo login/senha precisa ser preenchido </li>";
+	else:
+		//criptografando a senha
+		$senha = md5($senha);
+		//usuario: marta/senha:123456
+		$sql= "SELECT idUsuario FROM usuario WHERE dscEmailUsuario='$login' AND senhaUsuario='$senha'";
+		
+		$resultado = mysqli_query($connect,$sql);
+		
+		//numeros de linhas do resultado da query maior que 0 ou Se houver algum registro na tabela
+		if (mysqli_num_rows($resultado) > 0):
+			$dados=mysqli_fetch_array($resultado);
+			$_SESSION['logado'] = true;
+			$_SESSION['id_usuario']= $dados['id'];
+
+			header('Location: index.php');		
+		
+		else:
+			$erros[]="<li>Usuário e senha não conferem.</li>";
+		endif;
+	endif;	
+endif;	
+?>
+<?php
+if(!empty($erros)):
+	foreach($erros as $erro):
+		echo $erro;
+	endforeach;
+endif;
+?>
+<form action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST">
+
+Login: <input type="text" name="login"><br>
+Senha: <input type="password" name="senha"><br>
+<button type="submit" name="btn-entrar"> Entrar </button>
+</form>
+
+<?php 
+include_once "footer.php";
+?>
