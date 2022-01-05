7d1addc76c0bc11d8af2cade39885eac909c296c
01737 <59713688+LelioMarcos@users.noreply.github.com>
2022-01-05 14:53:45 -0300

    cadastro

--- cadastro.php
+++ cadastro.php
@@ -1,32 +1,53 @@
 <?php include_once "header.php"; 
 
 if (isset($_POST['btn-cadastrar'])) {
+    define('MULTIPART_BOUNDARY', '--------------------------'.microtime(true));
+
 	//echo "Clicou";
 	$erros = array();
 	//mysqli_escape_string - função que limpa os dados e evita sqlinjection e outros caracteres indevidos.
-	$username = htmlspecialchars($_POST['username']);
-    $email = htmlspecialchars($_POST['email']);
-    $password = htmlspecialchars($_POST['password']);
+    $password = htmlspecialchars($_POST['newPassword']);
 	$password_repete = htmlspecialchars($_POST['rpassword']);
-	$url="https://estorias-sem-h-crud.herokuapp.com/register.php";
+	$url="http://estorias-sem-h-crud.herokuapp.com/register.php";
 
     if ($password !== $password_repete) {
         $erros[]="<li> Senhas não batem.</li>";
     } else {
-        $data = array('newLogin' => $email, 'newPassword' => $password, 'newName' => $username);
+        $data = array('newLogin' => $email, 'newPassword' => $password, 'newName' => $username, 'newPhoto' => $file_contents);
+
+        $data = ""; 
+        $boundary = "---------------------".substr(md5(rand(0,32000)), 0, 10); 
+        
+        //Collect Postdata 
+        foreach($_POST as $key => $val) 
+        {
+            if ($key != "rpassword") {
+                $data .= "--$boundary\n"; 
+                $data .= "Content-Disposition: form-data; name=\"".$key."\"\n\n".$val."\n"; 
+            }
+        }
+
+        $data .= "--$boundary\n"; 
+    
+        $fileContents = file_get_contents($_FILES['newPhoto']['tmp_name']); 
+        
+        $data .= "Content-Disposition: form-data; name=\"newPhoto\"; filename=\"{$_FILES['newPhoto']['name']}\"\n"; 
+        $data .= "Content-Type: image/jpeg\n"; 
+        $data .= "Content-Transfer-Encoding: binary\n\n"; 
+        $data .= $fileContents."\n"; 
+        $data .= "--$boundary\n";
 
         $options = array(
             'http' => array(
-                'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+                'header'  => "Content-type: multipart/form-data; boundary=$boundary\r\n",
                 'method'  => 'POST',
-                'content' => http_build_query($data)
+                'content' => $data
             )
         );
+
         $context  = stream_context_create($options);
 
         $resultado = (array)json_decode(file_get_contents($url, false, $context));
-
-
         
         if ($resultado["success"] == 1) {    
             header('Location: login.php');	
@@ -40,18 +61,22 @@ if (isset($_POST['btn-cadastrar'])) {
 <article class="row">
     <div class="col s12 m6 push-m3 z-depth-5" id="cadastr">
         <h1>Cadastro</h1>
-        <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST">
-            <label for="username">Usuário:</label>
-            <input type="text" name="username">
+        <?php echo var_dump($erros)?>
+        <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST" enctype="multipart/form-data">
+            <label for="newName">Usuário:</label>
+            <input type="text" name="newName">
             <br>
-            <label for="email">Email:</label>
-            <input type="text" name="email">
+            <label for="newLogin">Email:</label>
+            <input type="text" name="newLogin">
             <br>
-            <label for="password">Senha:</label>
-            <input type="text" name="password">
+            <label for="newPassword">Senha:</label>
+            <input type="text" name="newPassword">
             <br>
             <label for="rpassword">Repita a Senha:</label>
             <input type="text" name="rpassword">
+            <br>
+            <label for="newPhoto">Foto de Perfil</label>
+            <input type="file" name="newPhoto" accept="image/png, image/jpeg">
             <br>
             <input type="submit" name="btn-cadastrar" value="Cadastrar" id="botao" class="waves-effect waves-light btn">
         </form>
